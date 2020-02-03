3d70634e16e0156be46d3e354ea4f761b2bf1843
Heorhii Siburov <g.siburov@gmail.com>
2020-02-03 12:47:14 -0800

    day3 expose

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,6 +5,8 @@ from .models import PersonalNote
 class NoteAdmin(admin.ModelAdmin):
 	readonly_fields=('created_at', 'last_modified')
 
+class PersonalNoteUser(admin.ModelAdmin):
+	readonly_fields=('created_at', 'last_modified')
 
 admin.site.register(Note, NoteAdmin)
-admin.site.register(PersonalNote)
+admin.site.register(PersonalNote, PersonalNoteUser)
